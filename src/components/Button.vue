f6a32bc7d4aac7de648ef956090eceffcbead4c0
majoruzb <egamberdimamatqulov95@gmail.com>
2021-11-21 17:28:01 +0500

    example was made with props and button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-   </div>
+    <button v-on:click="onClick()" :style="{ background: color }" class="btn">
+        {{text}}
+    </button>
 </template>
 
 <script>
 export default {
-    
+    name: "Button",
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('Bosildi');
+        }
+    }
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,21 @@
 <template>
     <header>
         <h1>{{title}}</h1>
+        <Button text="Click me" color="green"/>
     </header>
 </template>
 
 <script>
+import Button from './Button'
 
 export default {
     name: "Header",
     props: {
             title: String   
-        }
+        },
+    components: {
+        Button
+    }
 }
 </script>
 
